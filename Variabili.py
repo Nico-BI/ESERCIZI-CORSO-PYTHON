02a1013f777dbe060141630836bd8d3568f9d8f4
Nico-BI <nickjack89@live.it>
2026-06-08 12:34:29 +0200

    Esercizi con variabili

---
+++ Variabili.py
@@ -0,0 +1,17 @@
+#Crea una variabile con il tuo nome e stampala
+
+nome="Nicolas"
+print(nome)
+
+#Chiedi all'utente la sua età e stampala raddopiata
+
+età=int(input("Inserisci la tua età: "))
+età_raddoppiata= età * 2
+print(età_raddoppiata)
+
+#Crea due numeri e stampa la loro somma
+
+primo_numero=int(input("Inserisci il primo numero: "))
+secondo_numero=int(input("Inserisci il secondo numero: "))
+somma= primo_numero + secondo_numero
+print(somma)
