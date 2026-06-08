7b5f14b1b318d8b424f18f7ddb4092225ffeeb88
Nico-BI <nickjack89@live.it>
2026-06-08 13:14:24 +0200

    Esercizi con tipi di dati

---
+++ Tipi di dati.py
@@ -0,0 +1,32 @@
+#Esercizio 1
+
+x=7
+y=3
+
+somma= x + y
+print(somma,type(somma))
+
+
+#Esercizio 2
+
+prezzo=19.99
+quantità=3
+
+totale= prezzo* quantità
+print(totale,type(totale))
+
+
+#Esercizio 3
+
+numero=10
+print(type(numero>5))
+print(type(numero<=5))
+
+
+#Esercizio 4
+
+a=5
+b=2.5
+
+somma= a + b
+print(somma,type(somma))
