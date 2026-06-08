807db06fdfb6d2147a27e54eb02195b83afc5ce0
Nico-BI <nickjack89@live.it>
2026-06-08 14:01:31 +0200

    Esercizio con casting di una variabili

---
+++ Casting.py
@@ -0,0 +1,11 @@
+#Data una variabile convertila in diversi tipi di dato
+
+numero_intero=int(input("Inserisci un numero intero: "))
+
+numero_decimale= float(numero_intero)
+print(numero_decimale)
+
+numero_stringa= str(numero_decimale)
+print(numero_stringa,"È la sua conversione in una stringa")
+
+
